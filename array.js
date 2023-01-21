c5e2e3c51869c70d3f4bc44aa0fde0a64036d2af
shm_usuf <shmusuf@gmail.com>
2023-01-21 19:57:56 +0600

    dsa day 4

---
+++ array.js
@@ -0,0 +1,13 @@
+const arr = [1, 3, 4, 5, 6];
+
+/* console.log(arr[0]);
+console.log(arr.at(-1));
+console.log(arr[arr.length - 3]); */
+
+/* arr.push(10);
+arr.unshift(0);
+arr.pop();
+arr.shift(); */
+
+console.log(arr);
+console.log(arr.includes(4));
---
+++ obj.js
@@ -0,0 +1,9 @@
+const student = {
+  name: "Usuf Ali",
+  age: 26,
+};
+
+// console.log(student);
+
+console.log(student.name);
+console.log(student["age"]);
